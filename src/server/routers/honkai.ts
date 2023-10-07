392aa9c7911eca95512e40a4ac8be17ddb84eca0
Othi <mnpq.raven@gmail.com>
2023-10-07 15:54:31 +0700

    chore

--- src/server/routers/honkai.ts
+++ src/server/routers/honkai.ts
@@ -1,9 +1,13 @@
 import { db } from "@/lib/database";
 import { publicProcedure, router } from "../trpc";
 import { avatars } from "@/dbSchemas/avatar";
+import { items } from "@/dbSchemas/item";
 
 export const honkaiRouter = router({
   avatar: publicProcedure.query(async () => {
     return db.select().from(avatars).all();
+  }),
+  item: publicProcedure.query(async () => {
+    return db.select().from(items).all();
   }),
 });
